889b10f30aa6b62056d751fd6c04ff0b20183124
Fabi198 <fabianferrer42@gmail.com>
2023-12-07 18:18:59 -0300

    Minor changes

--- app/src/main/java/com/example/takeappointments/MainActivity.kt
+++ app/src/main/java/com/example/takeappointments/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.takeappointments
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.takeappointments.ui.theme.TakeAppointmentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,10 +25,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TakeAppointmentsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                Image(
+                    painter = painterResource(id = R.drawable.logo_full),
+                    contentDescription = "Example Image",
+                    modifier = Modifier
+                        .size(200.dp, 40.dp)
+                        .padding(12.dp, 10.dp, 0.dp, 0.dp),
+                    contentScale = ContentScale.Fit
+                )
             }
         }
     }
@@ -30,7 +42,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = Color.Red
     )
 }
 
